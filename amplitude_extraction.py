dc829c41cd2030056f1ea1348468cd1a0cac1177
Curtis Paterson <curtis.paterson1@gmail.com>
2025-01-09 14:06:52 +0000

    read .env file

--- amplitude_extraction.py
+++ amplitude_extraction.py
@@ -9,3 +9,8 @@ from dotenv import load_dotenv
 # Load .env file
 load_dotenv()
 
+# Read .env file
+api_key=os.getenv('AMP_API_KEY')
+api_secret_key=os.getenv('AMP_SECRET_KEY')
+print(api_key)
+print(api_secret_key)
