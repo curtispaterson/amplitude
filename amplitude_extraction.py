31ae15bcdca6879396aa1ca10f654e8060a2ff1b
Curtis Paterson <curtis.paterson1@gmail.com>
2025-01-09 13:54:37 +0000

    loading packages

---
+++ amplitude_extraction.py
@@ -0,0 +1,11 @@
+# API script to pull data from Amplitude Export API
+# https://amplitude.com/docs/apis/analytics/export
+
+# Load libraries
+import os
+import requests
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
